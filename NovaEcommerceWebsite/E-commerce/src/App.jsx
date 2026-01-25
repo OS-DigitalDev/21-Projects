25476e98225b115b78400acb694de8e6c0b29760
OS-DigitalDev <digitalsamurai.dev@gmail.com>
2026-01-25 16:55:15 +0530

    Update import statements and add BrowserRouter

--- NovaEcommerceWebsite/E-commerce/src/App.jsx
+++ NovaEcommerceWebsite/E-commerce/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Home from "./pages/Home";
-import Pdp from "./pages/pdp";
-import { Route, Routes } from "react-router-dom";
+import Pdp from "./pages/Pdp";
+import {BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
+    <BrowserRouter>
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/product/:id" element={<Pdp />} />
     </Routes>
+
+    </BrowserRouter>
   );
 };
 
